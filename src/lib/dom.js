6c791f7c021ec5b17609eca6d279e2be64df13cf
Bearman265 <43792727+Bearman265@users.noreply.github.com>
2018-11-26 10:58:31 +0000

    Meiri breyting á javascript og smávegis scss

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -23,37 +23,37 @@ function loadAllLectures(data, sida) {
     const elem = el('section');
 
     if (lecture.thumbnail) {
-      const image = el('img');
-      image.classList.add('fyrirlestrar__img');
-      image.setAttribute('src', lecture.thumbnail);
-      elem.appendChild(image);
+      const mynd = el('img');
+      mynd.classList.add('fyrirlestrar__img');
+      mynd.setAttribute('src', lecture.thumbnail);
+      elem.appendChild(mynd);
     }
 
     const flokkur = el('h2', lecture.category);
-    flokkur.classList.add('fyrirlestrar__category');
+    flokkur.classList.add('fyrirlestrar__flokkur');
     elem.appendChild(flokkur);
   
 
     const titill = el('h1', lecture.title);
-    titill.classList.add('fyrirlestrar__title');
+    titill.classList.add('fyrirlestrar__titill');
 
     const hak = el('img');
-    hak.classList.add('fyrirlestrar__checkbox');
-    hak.classList.add('fyrirlestrar__checkbox--hidden');
+    hak.classList.add('fyrirlestrar__hak');
+    hak.classList.add('fyrirlestrar__hak--hidden');
     hak.setAttribute('src', 'img/check.png');
 
     const wrap = el('div', titill, hak);
     wrap.classList.add('fyrirlestrar__titlewrap');
     elem.appendChild(wrap);
 
-    elem.classList.add('fyrirlestrar__section');
+    elem.classList.add('fyrirlestrar__hluti');
     lectures.appendChild(elem);
   }
 }
 
 function loadLecture(data, sida) {
-  const res = getObject(data);
-  const obj = res.content;
+  const res = getFyrirlestur(data);
+  const hlutur = res.content;
 
   // ef undefined, gera eh villudót
 
@@ -62,63 +62,74 @@ function loadLecture(data, sida) {
   const haus = sida.querySelector('.fyrirl-haus');
 
   const flokkur = el("h2",res.category)
-  flokkur.classList.add('fyrirl-haus__category');
+  flokkur.classList.add('fyrirl-haus__flokkur');
   haus.appendChild(flokkur);
 
   const titill = el("h1",res.title);
-  titill.classList.add('fyrirl-haus__title');
-  header.appendChild(title);
+  titill.classList.add('fyrirl-haus__titill');
+  haus.appendChild(titill);
 
-  for (const i in obj) {
-    const data = obj[i];
+  const hausMynd = el("img");
+  hausMynd.setAttribute('src', res.image);
+  hausMynd.classList.add('fyrirl-haus__mynd')
+  haus.appendChild(hausMynd);
+
+  
+
+  for (const i in hlutur) {
+    const data = hlutur[i];
     switch (data.type) {
       case 'text':
-        const text = el('p', data.data);
-        text.classList.add('fyrirlestur__text')
-        lecture.appendChild(text);
+        const pTexti = el('p', data.data);
+        pTexti.classList.add('fyrirlestur__texti')
+        lecture.appendChild(pTexti);
         break;
       case 'heading':
-        const heading = el('h1', data.data);
-        heading.classList.add('fyrirlestur__heading')
-        lecture.appendChild(heading);
+        const fyrirsogn = el('h1', data.data);
+        fyrirsogn.classList.add('fyrirlestur__fyrirsogn')
+        lecture.appendChild(fyrirsogn);
         break;
       case 'youtube':
         // todo
         break;
       case 'quote':
-        const quote = el('div', el('p', data.data));
-        quote.classList.add('fyrirlestur__quote');
-        lecture.appendChild(quote);
+        const tilvitnun = el('div', el('p', data.data));
+
+        tilvitnun.classList.add('fyrirlestur__tilvitnun');
+        lecture.appendChild(tilvitnun);
         break;
       case 'list':
-        const ul = el('ul');
-        ul.classList.add('fyrirlestur__list');
+        const listi = el('ul');
+        listi.classList.add('fyrirlestur__listi');
         for (const j in data.data) {
-          const li = el('li', data.data[j]);
-          li.classList.add('fyrirlestur__listitem');
-          ul.appendChild(li);
+          const stak = el('li', data.data[j]);
+          stak.classList.add('fyrirlestur__listiStak');
+          listi.appendChild(stak);
         }
-        lecture.appendChild(ul);
+        lecture.appendChild(listi);
         break;
       case 'image':
-        const image = el('img');
-        image.setAttribute('src', data.data);
-        image.classList.add('fyrirlestur__image')
-        lecture.appendChild(image);
+        const mynd = el('img');
+        mynd.setAttribute('src', data.data);
+        mynd.classList.add('fyrirlestur__mynd')
+
+        lecture.appendChild(mynd);
         break;
       case 'code':
-        const code = el('pre', el('p', data.data));
-        code.classList.add('fyrirlestur__code')
-        lecture.appendChild(code);
+        const kod = el('pre', el('p', data.data));
+        kod.classList.add('fyrirlestur__kodi')
+        lecture.appendChild(kod);
         break;
     }
   }
 }
 
-function getObject(data) {
-  const url_string = window.location.href;
-  const url = new URL(url_string);
-  const slug = url.searchParams.get("slug");
+function getFyrirlestur(data) {
+  const url1 = window.location.href;
+  const url2 = new URL(url1);
+
+  const slug = url2.searchParams.get("slug");
+
   return dataUrSlug(data, slug);
 }
 
